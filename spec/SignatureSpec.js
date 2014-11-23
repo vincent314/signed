12e970bc533e977cf8c846ab4d1a341ae2e3a53c
vincent <vincent314@hotmail.fr>
2014-11-23 21:55:38 +0100

    Hash algorithm in options

--- spec/SignatureSpec.js
+++ spec/SignatureSpec.js
@@ -1,13 +1,24 @@
 var signed = require('../lib/signed.js');
 
-var signature = signed({secret:'secret'});
 
 describe("Test signed", function () {
 
-    it("Should signe an URL",function(){
+    it("Should signe an URL with default algo", function () {
+        var signature = signed({secret: 'secret'});
+
         var newUrl = signature.sign('http://host:8080/anycontext');
 
         expect(newUrl).toMatch(new RegExp('http://host:8080/anycontext\\?signed=r:\\d+;\\w{32}'));
 
+
+        expect(signature.verifyUrl({host:'localhost',port:'8080',url:newUrl.split('host:8080')[1]})).toBeTrue();
+    });
+
+    it("Should signe an URL with sha1 algo", function () {
+        var signature = signed({secret: 'secret', algo: 'sha1'});
+
+        var newUrl = signature.sign('http://host:8080/anycontext');
+
+        expect(newUrl).toMatch(new RegExp('http://host:8080/anycontext\\?signed=r:\\d+;\\w{32}'));
     });
 });
